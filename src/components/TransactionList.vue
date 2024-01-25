9dea93c077f524dbfe15bf5fa997f77b98b0c1f4
NunomValdez <nunomvmf@hotmail.com>
2024-01-25 11:19:59 +0000

    passing props array to component

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,18 +1,27 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h3>Hisstory</h3>
+  <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-€400</span>
-      <button class="delete-btn">x</button>
-    </li>
-    <li class="minus">
-      Cash <span>-€200</span>
-      <button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Paycheck <span>€800</span>
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }}
+      <span>{{ transaction.amount }}€</span>
       <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+// in order to use props, we need to define it in the component, so it knows what to expect
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+})
+</script>
